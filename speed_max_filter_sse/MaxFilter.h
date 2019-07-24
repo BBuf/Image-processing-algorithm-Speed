196c7dd2cd6f9935992074e9330b1446cb316e9a
BBuf <1182563586@qq.com>
2019-07-24 17:55:54 +0800

    Add speed_max_filter_sse

--- speed_max_filter_sse/MaxFilter.h
+++ speed_max_filter_sse/MaxFilter.h
@@ -1,3 +1,135 @@
 #pragma once
 #include "Core.h"
-#include "MaxFilter.h"
+#include "Utility.h"
+
+// 函数供能: 在指定半径内，最大值”滤镜用周围像素的最高亮度值替换当前像素的亮度值。
+// 参数列表:
+// Src: 需要处理的源图像的数据结构
+// Dest: 保存处理后的图像的数据结构
+// Radius: 半径，有效范围
+// 说明：
+// 1、程序的执行时间和半径基本无关，但和图像内容有关
+// 2、Src和Dest可以相同，不同时执行速度很快
+// 3、对于各向异性的图像来说，执行速度很快，对于有大面积相同像素的图像，速度会慢一点
+
+IS_RET  MaxFilter(TMatrix *Src, TMatrix *Dest, int Radius)
+{
+	if (Src == NULL || Dest == NULL) return IS_RET_ERR_NULLREFERENCE;
+	if (Src->Data == NULL || Dest->Data == NULL) return IS_RET_ERR_NULLREFERENCE;
+	if (Src->Width != Dest->Width || Src->Height != Dest->Height || Src->Channel != Dest->Channel || Src->Depth != Dest->Depth || Src->WidthStep != Dest->WidthStep) return IS_RET_ERR_PARAMISMATCH;
+	if (Src->Depth != IS_DEPTH_8U || Dest->Depth != IS_DEPTH_8U) return IS_RET_ERR_NOTSUPPORTED;
+	if (Radius < 0 || Radius >= 127) return IS_RET_ERR_ARGUMENTOUTOFRANGE;
+
+	IS_RET Ret = IS_RET_OK;
+
+	if (Src->Data == Dest->Data)
+	{
+		TMatrix *Clone = NULL;
+		Ret = IS_CloneMatrix(Src, &Clone);
+		if (Ret != IS_RET_OK) return Ret;
+		Ret = MaxFilter(Clone, Dest, Radius);
+		IS_FreeMatrix(&Clone);
+		return Ret;
+	}
+	if (Src->Channel == 1)
+	{
+		TMatrix *Row = NULL, *Col = NULL;
+		unsigned char *LinePS, *LinePD;
+		int X, Y, K, Width = Src->Width, Height = Src->Height;
+		int *RowOffset, *ColOffSet;
+
+		unsigned short *ColHist = (unsigned short *)IS_AllocMemory(256 * (Width + 2 * Radius) * sizeof(unsigned short), true);
+		if (ColHist == NULL) { Ret = IS_RET_ERR_OUTOFMEMORY; goto Done8; }
+		unsigned short *Hist = (unsigned short *)IS_AllocMemory(256 * sizeof(unsigned short), true);
+		if (Hist == NULL) { Ret = IS_RET_ERR_OUTOFMEMORY; goto Done8; }
+		Ret = GetValidCoordinate(Width, Height, Radius, Radius, Radius, Radius, EdgeMode::Smear, &Row, &Col);		//	获取坐标偏移量
+		if (Ret != IS_RET_OK) goto Done8;
+
+		ColHist += Radius * 256;		RowOffset = ((int *)Row->Data) + Radius;
+		ColOffSet = ((int *)Col->Data) + Radius;		    	//	进行偏移以便操作
+
+		for (Y = 0; Y < Height; Y++)
+		{
+			if (Y == 0)											//	第一行的列直方图,要重头计算
+			{
+				for (K = -Radius; K <= Radius; K++)
+				{
+					LinePS = Src->Data + ColOffSet[K] * Src->WidthStep;
+					for (X = -Radius; X < Width + Radius; X++)
+					{
+						ColHist[X * 256 + LinePS[RowOffset[X]]]++;
+					}
+				}
+			}
+			else												//	其他行的列直方图，更新就可以了
+			{
+				LinePS = Src->Data + ColOffSet[Y - Radius - 1] * Src->WidthStep;
+				for (X = -Radius; X < Width + Radius; X++)		// 删除移出范围内的那一行的直方图数据
+				{
+					ColHist[X * 256 + LinePS[RowOffset[X]]]--;
+				}
+
+				LinePS = Src->Data + ColOffSet[Y + Radius] * Src->WidthStep;
+				for (X = -Radius; X < Width + Radius; X++)		// 增加进入范围内的那一行的直方图数据
+				{
+					ColHist[X * 256 + LinePS[RowOffset[X]]]++;
+				}
+			}
+
+			memset(Hist, 0, 256 * sizeof(unsigned short));		//	每一行直方图数据清零先
+
+			LinePD = Dest->Data + Y * Dest->WidthStep;
+
+			for (X = 0; X < Width; X++)
+			{
+				if (X == 0)
+				{
+					for (K = -Radius; K <= Radius; K++)			//	行第一个像素，需要重新计算	
+						HistgramAddShort(ColHist + K * 256, Hist);
+				}
+				else
+				{
+					/*	HistgramAddShort(ColHist + RowOffset[X + Radius] * 256, Hist);
+					HistgramSubShort(ColHist + RowOffset[X - Radius - 1] * 256, Hist);
+					*/
+					HistgramSubAddShort(ColHist + RowOffset[X - Radius - 1] * 256, ColHist + RowOffset[X + Radius] * 256, Hist);  //	行内其他像素，依次删除和增加就可以了
+				}
+				for (K = 255; K >= 0; K--)
+				{
+					if (Hist[K] != 0)
+					{
+						LinePD[X] = K;
+						break;
+					}
+				}
+			}
+		}
+		ColHist -= Radius * 256;		//	恢复偏移操作
+	Done8:
+		IS_FreeMatrix(&Row);
+		IS_FreeMatrix(&Col);
+		IS_FreeMemory(ColHist);
+		IS_FreeMemory(Hist);
+		return Ret;
+	}
+	else
+	{
+		TMatrix *Blue = NULL, *Green = NULL, *Red = NULL, *Alpha = NULL;			//	由于C变量如果不初始化，其值是随机值，可能会导致释放时的错误。
+		IS_RET Ret = SplitRGBA(Src, &Blue, &Green, &Red, &Alpha);
+		if (Ret != IS_RET_OK) goto Done24;
+		Ret = MaxFilter(Blue, Blue, Radius);
+		if (Ret != IS_RET_OK) goto Done24;
+		Ret = MaxFilter(Green, Green, Radius);
+		if (Ret != IS_RET_OK) goto Done24;
+		Ret = MaxFilter(Red, Red, Radius);
+		if (Ret != IS_RET_OK) goto Done24;											//	32位的Alpha不做任何处理，实际上32位的相关算法基本上是不能分通道处理的
+		CopyAlphaChannel(Src, Dest);
+		Ret = CombineRGBA(Dest, Blue, Green, Red, Alpha);
+	Done24:
+		IS_FreeMatrix(&Blue);
+		IS_FreeMatrix(&Green);
+		IS_FreeMatrix(&Red);
+		IS_FreeMatrix(&Alpha);
+		return Ret;
+	}
+}
--- speed_max_filter_sse/Utility.h
+++ speed_max_filter_sse/Utility.h
@@ -385,11 +385,160 @@ IS_RET SplitRGBA(TMatrix *Src, TMatrix **Blue, TMatrix **Green, TMatrix **Red, T
 		Ret = IS_CreateMatrix(Src->Width, Src->Height, Src->Depth, 1, Alpha);
 		if (Ret != IS_RET_OK) goto Done;
 	}
-
+	int X, Y, Block, Width = Src->Width, Height = Src->Height;
+	unsigned char *LinePS, *LinePB, *LinePG, *LinePR, *LinePA;
+	const int BlockSize = 8;
+	Block = Width / BlockSize;						//	8路并行,再多开几路，速度并没有增加了
+	if (Src->Channel == 3)
+	{
+		for (Y = 0; Y < Height; Y++)
+		{
+			LinePS = Src->Data + Y * Src->WidthStep;
+			LinePB = (*Blue)->Data + Y * (*Blue)->WidthStep;
+			LinePG = (*Green)->Data + Y * (*Green)->WidthStep;
+			LinePR = (*Red)->Data + Y * (*Red)->WidthStep;
+			for (X = 0; X < Block * BlockSize; X += BlockSize)			//	如果把LinePB全写在一起，速度反而慢一些
+			{
+				LinePB[0] = LinePS[0];		LinePG[0] = LinePS[1];		LinePR[0] = LinePS[2];
+				LinePB[1] = LinePS[3];		LinePG[1] = LinePS[4];		LinePR[1] = LinePS[5];
+				LinePB[2] = LinePS[6];		LinePG[2] = LinePS[7];		LinePR[2] = LinePS[8];
+				LinePB[3] = LinePS[9];		LinePG[3] = LinePS[10];		LinePR[3] = LinePS[11];
+				LinePB[4] = LinePS[12];		LinePG[4] = LinePS[13];		LinePR[4] = LinePS[14];
+				LinePB[5] = LinePS[15];		LinePG[5] = LinePS[16];		LinePR[5] = LinePS[17];
+				LinePB[6] = LinePS[18];		LinePG[6] = LinePS[19];		LinePR[6] = LinePS[20];
+				LinePB[7] = LinePS[21];		LinePG[7] = LinePS[22];		LinePR[7] = LinePS[23];
+				LinePB += 8;				LinePG += 8;				LinePR += 8;				LinePS += 24;
+			}
+			while (X < Width)
+			{
+				LinePB[0] = LinePS[0];		LinePG[0] = LinePS[1];		LinePR[0] = LinePS[2];
+				LinePB++;					LinePG++;					LinePR++;					LinePS += 3;
+				X++;
+			}
+		}
+	}
+	else if (Src->Channel == 4)
+	{
+		for (Y = 0; Y < Height; Y++)
+		{
+			LinePS = Src->Data + Y * Src->WidthStep;
+			LinePB = (*Blue)->Data + Y * (*Blue)->WidthStep;
+			LinePG = (*Green)->Data + Y * (*Green)->WidthStep;
+			LinePR = (*Red)->Data + Y * (*Red)->WidthStep;
+			LinePA = (*Alpha)->Data + Y * (*Alpha)->WidthStep;
+			for (X = 0; X < Block * BlockSize; X += BlockSize)
+			{
+				LinePB[0] = LinePS[0];		LinePG[0] = LinePS[1];		LinePR[0] = LinePS[2];		LinePA[0] = LinePS[3];
+				LinePB[1] = LinePS[4];		LinePG[1] = LinePS[5];		LinePR[1] = LinePS[6];		LinePA[1] = LinePS[7];
+				LinePB[2] = LinePS[8];		LinePG[2] = LinePS[9];		LinePR[2] = LinePS[10];		LinePA[2] = LinePS[11];
+				LinePB[3] = LinePS[12];		LinePG[3] = LinePS[13];		LinePR[3] = LinePS[14];		LinePA[3] = LinePS[15];
+				LinePB[4] = LinePS[16];		LinePG[4] = LinePS[17];		LinePR[4] = LinePS[18];		LinePA[4] = LinePS[19];
+				LinePB[5] = LinePS[20];		LinePG[5] = LinePS[21];		LinePR[5] = LinePS[22];		LinePA[5] = LinePS[23];
+				LinePB[6] = LinePS[24];		LinePG[6] = LinePS[25];		LinePR[6] = LinePS[26];		LinePA[6] = LinePS[27];
+				LinePB[7] = LinePS[28];		LinePG[7] = LinePS[29];		LinePR[7] = LinePS[30];		LinePA[7] = LinePS[31];
+				LinePB += 8;				LinePG += 8;				LinePR += 8;				LinePA += 8;				LinePS += 32;
+			}
+			while (X < Width)
+			{
+				LinePB[0] = LinePS[0];		LinePG[0] = LinePS[1];		LinePR[0] = LinePS[2];		LinePA[0] = LinePS[3];
+				LinePB++;					LinePG++;					LinePR++;					LinePA++;					LinePS += 4;
+				X++;
+			}
+		}
+	}
+	return IS_RET_OK;
 Done:
 	if (*Blue != NULL) IS_FreeMatrix(Blue);
 	if (*Green != NULL) IS_FreeMatrix(Green);
 	if (*Red != NULL) IS_FreeMatrix(Red);
 	if (*Alpha != NULL) IS_FreeMatrix(Alpha);
 	return Ret;
+}
+
+// 函数20: 将R,G,B,A单通道的图像合并为彩色的图像
+// 参数列表:
+// Dest: 合并处理后的图像的数据结构
+// Blue: Blue通道图像的数据结构
+// Green: Green通道图像的数据结构
+// Red: Red通道图像的数据结构
+// Alpha: Alpha通道图像的数据结构
+IS_RET CombineRGBA(TMatrix *Dest, TMatrix *Blue, TMatrix *Green, TMatrix *Red, TMatrix *Alpha)
+{
+	if (Dest == NULL || Blue == NULL || Green == NULL || Red == NULL) return IS_RET_ERR_NULLREFERENCE;
+	if (Dest->Data == NULL || Blue->Data == NULL || Green->Data == NULL || Red->Data == NULL) return IS_RET_ERR_NULLREFERENCE;
+	if ((Dest->Channel != 3 && Dest->Channel != 4) || Blue->Channel != 1 || Green->Channel != 1 || Red->Channel != 1) return IS_RET_ERR_PARAMISMATCH;
+	if (Dest->Width != Blue->Width || Dest->Width != Green->Width || Dest->Width != Red->Width || Dest->Width != Blue->Width)  return IS_RET_ERR_PARAMISMATCH;
+	if (Dest->Height != Blue->Height || Dest->Height != Green->Height || Dest->Height != Red->Height || Dest->Height != Blue->Height)  return IS_RET_ERR_PARAMISMATCH;
+
+	if (Dest->Channel == 4)
+	{
+		if (Alpha == NULL) return IS_RET_ERR_NULLREFERENCE;
+		if (Alpha->Data == NULL) return IS_RET_ERR_NULLREFERENCE;
+		if (Alpha->Channel != 1) return IS_RET_ERR_PARAMISMATCH;
+		if (Dest->Width != Alpha->Width || Dest->Height != Alpha->Height) return IS_RET_ERR_PARAMISMATCH;
+	}
+
+	int X, Y, Block, Width = Dest->Width, Height = Dest->Height;
+	unsigned char *LinePD, *LinePB, *LinePG, *LinePR, *LinePA;
+	const int BlockSize = 8;
+	Block = Width / BlockSize;						//	8路并行,再多开几路，速度并没有增加了
+
+	if (Dest->Channel == 3)
+	{
+		for (Y = 0; Y < Height; Y++)
+		{
+			LinePD = Dest->Data + Y * Dest->WidthStep;
+			LinePB = Blue->Data + Y * Blue->WidthStep;
+			LinePG = Green->Data + Y * Green->WidthStep;
+			LinePR = Red->Data + Y * Red->WidthStep;
+			for (X = 0; X < Block * BlockSize; X += BlockSize)				//	如果把LinePB全写在一起，速度区别不大
+			{
+				LinePD[0] = LinePB[0];		LinePD[1] = LinePG[0];		LinePD[2] = LinePR[0];
+				LinePD[3] = LinePB[1];		LinePD[4] = LinePG[1];		LinePD[5] = LinePR[1];
+				LinePD[6] = LinePB[2];		LinePD[7] = LinePG[2];		LinePD[8] = LinePR[2];
+				LinePD[9] = LinePB[3];		LinePD[10] = LinePG[3];		LinePD[11] = LinePR[3];
+				LinePD[12] = LinePB[4];		LinePD[13] = LinePG[4];		LinePD[14] = LinePR[4];
+				LinePD[15] = LinePB[5];		LinePD[16] = LinePG[5];		LinePD[17] = LinePR[5];
+				LinePD[18] = LinePB[6];		LinePD[19] = LinePG[6];		LinePD[20] = LinePR[6];
+				LinePD[21] = LinePB[7];		LinePD[22] = LinePG[7];		LinePD[23] = LinePR[7];
+				LinePB += 8;				LinePG += 8;				LinePR += 8;				LinePD += 24;
+			}
+			while (X < Width)
+			{
+				LinePD[0] = LinePB[0];		LinePD[1] = LinePG[0];		LinePD[2] = LinePR[0];
+				LinePB++;					LinePG++;					LinePR++;					LinePD += 3;
+				X++;
+			}
+		}
+	}
+	else if (Dest->Channel == 4)
+	{
+		for (Y = 0; Y < Height; Y++)
+		{
+			LinePD = Dest->Data + Y * Dest->WidthStep;
+			LinePB = Blue->Data + Y * Blue->WidthStep;
+			LinePG = Green->Data + Y * Green->WidthStep;
+			LinePR = Red->Data + Y * Red->WidthStep;
+			LinePA = Alpha->Data + Y * Alpha->WidthStep;
+			for (X = 0; X < Block * BlockSize; X += BlockSize)
+			{
+				LinePD[0] = LinePB[0];		LinePD[1] = LinePG[0];		LinePD[2] = LinePR[0];		LinePD[3] = LinePA[0];
+				LinePD[4] = LinePB[1];		LinePD[5] = LinePG[1];		LinePD[6] = LinePR[1];		LinePD[7] = LinePA[1];
+				LinePD[8] = LinePB[2];		LinePD[9] = LinePG[2];		LinePD[10] = LinePR[2];		LinePD[11] = LinePA[2];
+				LinePD[12] = LinePB[3];		LinePD[13] = LinePG[3];		LinePD[14] = LinePR[3];		LinePD[15] = LinePA[3];
+				LinePD[16] = LinePB[4];		LinePD[17] = LinePG[4];		LinePD[18] = LinePR[4];		LinePD[19] = LinePA[4];
+				LinePD[20] = LinePB[5];		LinePD[21] = LinePG[5];		LinePD[22] = LinePR[5];		LinePD[23] = LinePA[5];
+				LinePD[24] = LinePB[6];		LinePD[25] = LinePG[6];		LinePD[26] = LinePR[6];		LinePD[27] = LinePA[6];
+				LinePD[28] = LinePB[7];		LinePD[29] = LinePG[7];		LinePD[30] = LinePR[7];		LinePD[31] = LinePA[7];
+				LinePB += 8;				LinePG += 8;				LinePR += 8;				LinePA += 8;				LinePD += 32;
+			}
+			while (X < Width)
+			{
+				LinePD[0] = LinePB[0];		LinePD[1] = LinePG[0];		LinePD[2] = LinePR[0];		LinePD[3] = LinePA[0];
+				LinePB++;					LinePG++;					LinePD++;					LinePA++;					LinePD += 4;
+				X++;
+			}
+		}
+	}
+	return IS_RET_OK;
 }
