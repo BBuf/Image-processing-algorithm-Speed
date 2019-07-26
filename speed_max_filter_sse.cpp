3a7eeb79db6032d08082e35fabbb074180d688e9
BBuf <1182563586@qq.com>
2019-07-26 11:44:48 +0800

    Add speed_max_filter_sse.cpp

--- speed_max_filter_sse.cpp
+++ speed_max_filter_sse.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace cv;
 
-void MaxFilter(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride, int Channel, int Radius) {
+void MaxFilter_SSE(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride, int Channel, int Radius) {
 	TMatrix a, b;
 	TMatrix *p1 = &a, *p2 = &b;
 	TMatrix **p3 = &p1, **p4 = &p2;
@@ -17,6 +17,29 @@ void MaxFilter(unsigned char *Src, unsigned char *Dest, int Width, int Height, i
 	MaxFilter(p1, p2, Radius);
 }
 
+Mat MaxFilter(Mat src, int radius) {
+	int row = src.rows;
+	int col = src.cols;
+	int border = (radius - 1) / 2;
+	Mat dst(row, col, CV_8UC3);
+	printf("success\n");
+	for (int i = border; i + border < row; i++) {
+		for (int j = border; j + border < col; j++) {
+			for (int k = 0; k < 3; k++) {
+				int val = src.at<Vec3b>(i, j)[k];
+				for (int x = -border; x <= border; x++) {
+					for (int y = -border; y <= border; y++) {
+						val = max(val, (int)src.at<Vec3b>(i + x, j + y)[k]);
+					}
+				}
+				dst.at<Vec3b>(i, j)[k] = val;
+			}
+		}
+	}
+	printf("success\n");
+	return dst;
+}
+
 int main() {
 	Mat src = imread("F:\\car.jpg");
 	int Height = src.rows;
@@ -24,14 +47,15 @@ int main() {
 	unsigned char *Src = src.data;
 	unsigned char *Dest = new unsigned char[Height * Width * 3];
 	int Stride = Width * 3;
-	int Radius = 0;
+	int Radius = 11;
 	int64 st = cvGetTickCount();
-	for (int i = 0; i <50; i++) {
-		MaxFilter(Src, Dest, Width, Height, Stride, 3, Radius);
+	for (int i = 0; i <10; i++) {
+		Mat temp = MaxFilter(src, Radius);
+		//MaxFilter_SSE(Src, Dest, Width, Height, Stride, 3, Radius);
 	}
-	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 20;
+	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 100;
 	printf("%.5f\n", duration);
-	MaxFilter(Src, Dest, Width, Height, Stride, 3, Radius);
+	MaxFilter_SSE(Src, Dest, Width, Height, Stride, 3, Radius);
 	Mat dst(Height, Width, CV_8UC3, Dest);
 	imshow("origin", src);
 	imshow("result", dst);
