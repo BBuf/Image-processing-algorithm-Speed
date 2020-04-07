4710fb2f9334b66a9245987e3a168113ad0a6ba4
BBuf <1182563586@qq.com>
2020-04-07 18:16:22 +0800

    Add speed_rgb2gray_sse.cpp

--- speed_rgb2gray_sse.cpp
+++ speed_rgb2gray_sse.cpp
@@ -1,6 +1,6 @@
-#include "stdafx.h"
 #include <opencv2/opencv.hpp>
-
+#include <future>
+#include <immintrin.h>
 using namespace std;
 using namespace cv;
 
@@ -49,6 +49,7 @@ void RGB2Y_2(unsigned char *Src, unsigned char *Dest, int Width, int Height, int
 	}
 }
 
+//RGB2Y_3 一次处理12个像素
 void RGB2Y_3(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
 	const int B_WT = int(0.114 * 256 + 0.5);
 	const int G_WT = int(0.587 * 256 + 0.5);
@@ -90,7 +91,7 @@ void RGB2Y_3(unsigned char *Src, unsigned char *Dest, int Width, int Height, int
 			__m128i accumL = _mm_or_si128(shftaL, shftbL);//25
 			__m128i accumH = _mm_or_si128(shftaH, shftbH);//26
 			__m128i h3 = _mm_or_si128(accumL, accumH);//27
-			//__m128i h3 = _mm_blendv_epi8(accumL, accumH, _mm_setr_epi8(0, 0, 0, -1, -1, -1, 0, 0, 0, -1, -1, -1, 1, 1, 1, 1));
+													  //__m128i h3 = _mm_blendv_epi8(accumL, accumH, _mm_setr_epi8(0, 0, 0, -1, -1, -1, 0, 0, 0, -1, -1, -1, 1, 1, 1, 1));
 			_mm_storeu_si128((__m128i *)(LinePD + X), h3);
 		}
 		for (; X < Width; X++, LinePS += 3) {
@@ -99,7 +100,125 @@ void RGB2Y_3(unsigned char *Src, unsigned char *Dest, int Width, int Height, int
 	}
 }
 
+void RGB2Y_4(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
+	const int B_WT = int(0.114 * 256 + 0.5);
+	const int G_WT = int(0.587 * 256 + 0.5);
+	const int R_WT = 256 - B_WT - G_WT; // int(0.299 * 256 + 0.5)
 
+	for (int Y = 0; Y < Height; Y++) {
+		unsigned char *LinePS = Src + Y * Stride;
+		unsigned char *LinePD = Dest + Y * Width;
+		int X = 0;
+		for (; X < Width - 15; X += 15, LinePS += 45)
+		{
+			__m128i p1aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 0))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT)); //1
+			__m128i p2aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 1))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT)); //2
+			__m128i p3aL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 2))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT)); //3
+
+			__m128i p1aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 8))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
+			__m128i p2aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 9))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
+			__m128i p3aH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 10))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
+
+			__m128i p1bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 18))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
+			__m128i p2bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 19))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
+			__m128i p3bL = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 20))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
+
+			__m128i p1bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 26))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
+			__m128i p2bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 27))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
+			__m128i p3bH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 28))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
+
+			__m128i p1cH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 36))), _mm_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT));
+			__m128i p2cH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 37))), _mm_setr_epi16(G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT));
+			__m128i p3cH = _mm_mullo_epi16(_mm_cvtepu8_epi16(_mm_loadu_si128((__m128i *)(LinePS + 38))), _mm_setr_epi16(R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT));
+
+			__m128i sumaL = _mm_add_epi16(p3aL, _mm_add_epi16(p1aL, p2aL));
+			__m128i sumaH = _mm_add_epi16(p3aH, _mm_add_epi16(p1aH, p2aH));
+			__m128i sumbL = _mm_add_epi16(p3bL, _mm_add_epi16(p1bL, p2bL));
+			__m128i sumbH = _mm_add_epi16(p3bH, _mm_add_epi16(p1bH, p2bH));
+			__m128i sumcH = _mm_add_epi16(p3cH, _mm_add_epi16(p1cH, p2cH));
+
+			__m128i sclaL = _mm_srli_epi16(sumaL, 8);
+			__m128i sclaH = _mm_srli_epi16(sumaH, 8);
+			__m128i sclbL = _mm_srli_epi16(sumbL, 8);
+			__m128i sclbH = _mm_srli_epi16(sumbH, 8);
+			__m128i sclcH = _mm_srli_epi16(sumcH, 8);
+
+			__m128i shftaL = _mm_shuffle_epi8(sclaL, _mm_setr_epi8(0, 6, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
+			__m128i shftaH = _mm_shuffle_epi8(sclaH, _mm_setr_epi8(-1, -1, -1, 2, 8, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1));
+			__m128i shftbL = _mm_shuffle_epi8(sclbL, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, 0, 6, 12, -1, -1, -1, -1, -1, -1, -1));
+			__m128i shftbH = _mm_shuffle_epi8(sclbH, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 8, 14, -1, -1, -1, -1));
+			__m128i shftcH = _mm_shuffle_epi8(sclcH, _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 6, 12, -1));
+			__m128i accumL = _mm_or_si128(shftaL, shftbL);
+			__m128i accumH = _mm_or_si128(shftaH, shftbH);
+			__m128i h3 = _mm_or_si128(accumL, accumH);
+			h3 = _mm_or_si128(h3, shftcH);
+			_mm_storeu_si128((__m128i *)(LinePD + X), h3);
+		}
+	}
+}
+
+void debug(__m128i var) {
+	uint8_t *val = (uint8_t*)&var;//can also use uint32_t instead of 16_t 
+	printf("Numerical: %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i\n",
+		val[0], val[1], val[2], val[3], val[4], val[5],
+		val[6], val[7], val[8], val[9], val[10], val[11], val[12], val[13],
+		val[14], val[15]);
+}
+
+void debug2(__m256i var) {
+	uint8_t *val = (uint8_t*)&var;//can also use uint32_t instead of 16_t 
+	printf("Numerical: %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i\n",
+		val[0], val[1], val[2], val[3], val[4], val[5],
+		val[6], val[7], val[8], val[9], val[10], val[11], val[12], val[13],
+		val[14], val[15], val[16], val[17], val[18], val[19], val[20], val[21], val[22], val[23], val[24], val[25], val[26], val[27], 
+		val[28], val[29], val[30], val[31]);
+}
+
+// AVX2
+constexpr double B_WEIGHT = 0.114;
+constexpr double G_WEIGHT = 0.587;
+constexpr double R_WEIGHT = 0.299;
+constexpr uint16_t B_WT = static_cast<uint16_t>(32768.0 * B_WEIGHT + 0.5);
+constexpr uint16_t G_WT = static_cast<uint16_t>(32768.0 * G_WEIGHT + 0.5);
+constexpr uint16_t R_WT = static_cast<uint16_t>(32768.0 * R_WEIGHT + 0.5);
+static const __m256i weight_vec = _mm256_setr_epi16(B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT, G_WT, R_WT, B_WT);
+
+void  _RGB2Y(unsigned char* Src, const int32_t Width, const int32_t start_row, const int32_t thread_stride, const int32_t Stride, unsigned char* Dest)
+{
+	for (int Y = start_row; Y < start_row + thread_stride; Y++)
+	{
+		//Sleep(1);
+		unsigned char *LinePS = Src + Y * Stride;
+		unsigned char *LinePD = Dest + Y * Width;
+		int X = 0;
+		for (; X < Width - 10; X += 10, LinePS += 30)
+		{
+			printf("%d\n", X);
+			__m256i temp;
+			__m128i tempp = _mm_loadu_si128((const __m128i*)(LinePS + 0));
+			debug(tempp);
+			temp = _mm256_cvtepu8_epi16(tempp);
+			__m256i in1 = _mm256_mulhrs_epi16(temp, weight_vec);
+			tempp = _mm_loadu_si128((const __m128i*)(LinePS + 15));
+			debug(tempp);
+			temp = _mm256_cvtepu8_epi16(tempp);
+			__m256i in2 = _mm256_mulhrs_epi16(temp, weight_vec);
+			__m256i mul = _mm256_packus_epi16(in1, in2);
+			__m256i b1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(0, 3, 6, -1, -1, -1, 11, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 4, 7, -1, -1, 9, 12, -1, -1, -1, -1, -1, -1));
+			__m256i g1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(1, 4, 7, -1, -1, 9, 12, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2, 5, -1, -1, -1, 10, 13, -1, -1, -1, -1, -1, -1));
+			__m256i r1 = _mm256_shuffle_epi8(mul, _mm256_setr_epi8(2, 5, -1, -1, -1, 10, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 3, 6, -1, -1, 8, 11, 14, -1, -1, -1, -1, -1, -1));
+			__m256i accum = _mm256_adds_epu8(r1, _mm256_adds_epu8(b1, g1));
+			__m128i h3 = _mm_adds_epu8(_mm256_castsi256_si128(accum), _mm256_extracti128_si256(accum, 1));
+			_mm_storeu_si128((__m128i *)(LinePD + X), h3);
+		}
+	}
+}
+
+
+void RGB2Y_5(unsigned char *Src, unsigned char *Dest, int width, int height, int stride)
+{
+	_RGB2Y(Src, width, 0, height, stride, Dest);
+}
 
 int main() {
 	Mat src = imread("F:\\car.jpg");
@@ -110,12 +229,12 @@ int main() {
 	int Stride = Width * 3;
 	int Radius = 11;
 	int64 st = cvGetTickCount();
-	for (int i = 0; i < 100; i++) {
-		RGB2Y_3(Src, Dest, Width, Height, Stride);
-	}
+	/*for (int i = 0; i < 10; i++) {
+		RGB2Y_5(Src, Dest, Width, Height, Stride);
+	}*/
 	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 10;
 	printf("%.5f\n", duration);
-	RGB2Y(Src, Dest, Width, Height, Stride);
+	RGB2Y_5(Src, Dest, Width, Height, Stride);
 	Mat dst(Height, Width, CV_8UC1, Dest);
 	imshow("origin", src);
 	imshow("result", dst);
