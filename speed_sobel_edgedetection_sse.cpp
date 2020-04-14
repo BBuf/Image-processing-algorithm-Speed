14fcb20a86f70ed6ced6ab37bf9411c9ca2f8f2a
BBuf <1182563586@qq.com>
2020-04-14 14:16:51 +0800

    Add speed_sobel_edgedetection.cpp

--- speed_sobel_edgedetection_sse.cpp
+++ speed_sobel_edgedetection_sse.cpp
@@ -127,7 +127,92 @@ void Sobel_INT(unsigned char *Src, unsigned char *Dest, int Width, int Height, i
 	free(RowCopy);
 }
 
-void Sobel_SSE(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
+void Sobel_SSE1(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
+	int Channel = Stride / Width;
+	unsigned char *RowCopy = (unsigned char*)malloc((Width + 2) * 3 * Channel);
+	unsigned char *First = RowCopy;
+	unsigned char *Second = RowCopy + (Width + 2) * Channel;
+	unsigned char *Third = RowCopy + (Width + 2) * 2 * Channel;
+	//拷贝第二行数据，边界值填充
+	memcpy(Second, Src, Channel);
+	memcpy(Second + Channel, Src, Width*Channel);
+	memcpy(Second + (Width + 1)*Channel, Src + (Width - 1)*Channel, Channel);
+	//第一行和第二行一样
+	memcpy(First, Second, (Width + 2) * Channel);
+	//拷贝第三行数据，边界值填充
+	memcpy(Third, Src + Stride, Channel);
+	memcpy(Third + Channel, Src + Stride, Width * Channel);
+	memcpy(Third + (Width + 1) * Channel, Src + Stride + (Width - 1) * Channel, Channel);
+
+	int BlockSize = 8, Block = (Width * Channel) / BlockSize;
+
+	unsigned char Table[65026];
+	for (int Y = 0; Y < 65026; Y++) Table[Y] = (sqrtf(Y + 0.0f) + 0.5f);
+	for (int Y = 0; Y < Height; Y++) {
+		unsigned char *LinePS = Src + Y * Stride;
+		unsigned char *LinePD = Dest + Y * Stride;
+		if (Y != 0) {
+			unsigned char *Temp = First;
+			First = Second;
+			Second = Third;
+			Third = Temp;
+		}
+		if (Y == Height - 1) {
+			memcpy(Third, Second, (Width + 2) * Channel);
+		}
+		else {
+			memcpy(Third, Src + (Y + 1) * Stride, Channel);
+			memcpy(Third + Channel, Src + (Y + 1) * Stride, Width * Channel);                            //    由于备份了前面一行的数据，这里即使Src和Dest相同也是没有问题的
+			memcpy(Third + (Width + 1) * Channel, Src + (Y + 1) * Stride + (Width - 1) * Channel, Channel);
+		}
+		if (Channel == 1) {
+			for (int X = 0; X < Width; X++)
+			{
+				int GX = First[X] - First[X + 2] + (Second[X] - Second[X + 2]) * 2 + Third[X] - Third[X + 2];
+				int GY = First[X] + First[X + 2] + (First[X + 1] - Third[X + 1]) * 2 - Third[X] - Third[X + 2];
+				//LinePD[X] = Table[min(GX * GX + GY * GY, 65025)];
+			}
+		}
+		else
+		{
+			__m128i Zero = _mm_setzero_si128();
+			for (int X = 0; X < Block * BlockSize; X += BlockSize)
+			{
+				__m128i FirstP0 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(First + X)), Zero);
+				__m128i FirstP1 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(First + X + 3)), Zero);
+				__m128i FirstP2 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(First + X + 6)), Zero);
+
+				__m128i SecondP0 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(Second + X)), Zero);
+				__m128i SecondP2 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(Second + X + 6)), Zero);
+
+				__m128i ThirdP0 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(Third + X)), Zero);
+				__m128i ThirdP1 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(Third + X + 3)), Zero);
+				__m128i ThirdP2 = _mm_unpacklo_epi8(_mm_loadl_epi64((__m128i *)(Third + X + 6)), Zero);
+
+				__m128i GX16 = _mm_abs_epi16(_mm_add_epi16(_mm_add_epi16(_mm_sub_epi16(FirstP0, FirstP2), _mm_slli_epi16(_mm_sub_epi16(SecondP0, SecondP2), 1)), _mm_sub_epi16(ThirdP0, ThirdP2)));
+				__m128i GY16 = _mm_abs_epi16(_mm_sub_epi16(_mm_add_epi16(_mm_add_epi16(FirstP0, FirstP2), _mm_slli_epi16(_mm_sub_epi16(FirstP1, ThirdP1), 1)), _mm_add_epi16(ThirdP0, ThirdP2)));
+
+				__m128i GX32L = _mm_unpacklo_epi16(GX16, Zero);
+				__m128i GX32H = _mm_unpackhi_epi16(GX16, Zero);
+				__m128i GY32L = _mm_unpacklo_epi16(GY16, Zero);
+				__m128i GY32H = _mm_unpackhi_epi16(GY16, Zero);
+				__m128i ResultL = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_add_epi32(_mm_mullo_epi32(GX32L, GX32L), _mm_mullo_epi32(GY32L, GY32L)))));
+				__m128i ResultH = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_add_epi32(_mm_mullo_epi32(GX32H, GX32H), _mm_mullo_epi32(GY32H, GY32H)))));
+				_mm_storel_epi64((__m128i *)(LinePD + X), _mm_packus_epi16(_mm_packus_epi32(ResultL, ResultH), Zero));
+			}
+
+			for (int X = Block * BlockSize; X < Width * 3; X++)
+			{
+				int GX = First[X] - First[X + 6] + (Second[X] - Second[X + 6]) * 2 + Third[X] - Third[X + 6];
+				int GY = First[X] + First[X + 6] + (First[X + 3] - Third[X + 3]) * 2 - Third[X] - Third[X + 6];
+				LinePD[X] = IM_ClampToByte(sqrtf(GX * GX + GY * GY + 0.0F));
+			}
+		}
+	}
+	free(RowCopy);
+}
+
+void Sobel_SSE2(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
 	int Channel = Stride / Width;
 	unsigned char *RowCopy = (unsigned char*)malloc((Width + 2) * 3 * Channel);
 	unsigned char *First = RowCopy;
@@ -197,16 +282,6 @@ void Sobel_SSE(unsigned char *Src, unsigned char *Dest, int Width, int Height, i
 
 				__m128i ResultL = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_madd_epi16(GXYL, GXYL))));
 				__m128i ResultH = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_madd_epi16(GXYH, GXYH))));
-
-				/*__m128i GX32L = _mm_unpacklo_epi16(GX16, Zero);
-				__m128i GX32H = _mm_unpackhi_epi16(GX16, Zero);
-				__m128i GY32L = _mm_unpacklo_epi16(GY16, Zero);
-				__m128i GY32H = _mm_unpackhi_epi16(GY16, Zero);
-
-				__m128i ResultL = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_add_epi32(_mm_mullo_epi32(GX32L, GX32L), _mm_mullo_epi32(GY32L, GY32L)))));
-				__m128i ResultH = _mm_cvtps_epi32(_mm_sqrt_ps(_mm_cvtepi32_ps(_mm_add_epi32(_mm_mullo_epi32(GX32H, GX32H), _mm_mullo_epi32(GY32H, GY32H)))));
-
-				_mm_storel_epi64((__m128i *)(LinePD + X), _mm_packus_epi16(_mm_packus_epi32(ResultL, ResultH), Zero));*/
 				_mm_storel_epi64((__m128i *)(LinePD + X), _mm_packus_epi16(_mm_packus_epi32(ResultL, ResultH), Zero));
 			}
 
@@ -221,6 +296,99 @@ void Sobel_SSE(unsigned char *Src, unsigned char *Dest, int Width, int Height, i
 	free(RowCopy);
 }
 
+void Sobel_AVX1(unsigned char *Src, unsigned char *Dest, int Width, int Height, int Stride) {
+	int Channel = Stride / Width;
+	unsigned char *RowCopy = (unsigned char*)malloc((Width + 2) * 3 * Channel);
+	unsigned char *First = RowCopy;
+	unsigned char *Second = RowCopy + (Width + 2) * Channel;
+	unsigned char *Third = RowCopy + (Width + 2) * 2 * Channel;
+	//拷贝第二行数据，边界值填充
+	memcpy(Second, Src, Channel);
+	memcpy(Second + Channel, Src, Width*Channel);
+	memcpy(Second + (Width + 1)*Channel, Src + (Width - 1)*Channel, Channel);
+	//第一行和第二行一样
+	memcpy(First, Second, (Width + 2) * Channel);
+	//拷贝第三行数据，边界值填充
+	memcpy(Third, Src + Stride, Channel);
+	memcpy(Third + Channel, Src + Stride, Width * Channel);
+	memcpy(Third + (Width + 1) * Channel, Src + Stride + (Width - 1) * Channel, Channel);
+
+	int BlockSize = 16, Block = (Width * Channel) / BlockSize;
+
+	unsigned char Table[65026];
+	for (int Y = 0; Y < 65026; Y++) Table[Y] = (sqrtf(Y + 0.0f) + 0.5f);
+	for (int Y = 0; Y < Height; Y++) {
+		unsigned char *LinePS = Src + Y * Stride;
+		unsigned char *LinePD = Dest + Y * Stride;
+		if (Y != 0) {
+			unsigned char *Temp = First;
+			First = Second;
+			Second = Third;
+			Third = Temp;
+		}
+		if (Y == Height - 1) {
+			memcpy(Third, Second, (Width + 2) * Channel);
+		}
+		else {
+			memcpy(Third, Src + (Y + 1) * Stride, Channel);
+			memcpy(Third + Channel, Src + (Y + 1) * Stride, Width * Channel);                            //    由于备份了前面一行的数据，这里即使Src和Dest相同也是没有问题的
+			memcpy(Third + (Width + 1) * Channel, Src + (Y + 1) * Stride + (Width - 1) * Channel, Channel);
+		}
+		if (Channel == 1) {
+			for (int X = 0; X < Width; X++)
+			{
+				int GX = First[X] - First[X + 2] + (Second[X] - Second[X + 2]) * 2 + Third[X] - Third[X + 2];
+				int GY = First[X] + First[X + 2] + (First[X + 1] - Third[X + 1]) * 2 - Third[X] - Third[X + 2];
+				//LinePD[X] = Table[min(GX * GX + GY * GY, 65025)];
+			}
+		}
+		else
+		{
+			__m256i Zero = _mm256_setzero_si256();
+			for (int X = 0; X < Block * BlockSize; X += BlockSize)
+			{
+				__m256i FirstP0 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(First + X)));
+				__m256i FirstP1 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(First + X + 3)));
+				__m256i FirstP2 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(First + X + 6)));
+
+				__m256i SecondP0 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(Second + X)));
+				__m256i SecondP2 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(Second + X + 6)));
+
+				__m256i ThirdP0 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(Third + X)));
+				__m256i ThirdP1 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(Third + X + 3)));
+				__m256i ThirdP2 = _mm256_cvtepu8_epi16(_mm_loadu_si128((const __m128i*)(Third + X + 6)));
+
+				//GX0	GX1	    GX2    GX3    GX4    GX5    GX6    GX7     GX8	 GX9	 GX10    GX11    GX12    GX13    GX14    GX15
+				__m256i GX16 = _mm256_abs_epi16(_mm256_adds_epi16(_mm256_adds_epi16(_mm256_subs_epi16(FirstP0, FirstP2), _mm256_slli_epi16(_mm256_subs_epi16(SecondP0, SecondP2), 1)), _mm256_subs_epi16(ThirdP0, ThirdP2)));
+				//GY0   GY1     GY2    GY3    GY4    GY5    GY6    GY7     GY8   GY9     GY10    GY11    GY12    GY13    GY14    GY15
+				__m256i GY16 = _mm256_abs_epi16(_mm256_subs_epi16(_mm256_adds_epi16(_mm256_adds_epi16(FirstP0, FirstP2), _mm256_slli_epi16(_mm256_subs_epi16(FirstP1, ThirdP1), 1)), _mm256_adds_epi16(ThirdP0, ThirdP2)));
+				//GX0　　GY0　　GX1　　GY1　　GX2　　GY2　　GX3　　GY3    GX4    GY4     GX5     GY5      GX6     GY6     GX7     GY7
+				__m256i GXYL = _mm256_unpacklo_epi16(GX16, GY16);
+				//GX8　　GY8　　GX9　　GY9　　GX10　GY10　　GX11　GY11    GX12   GY12    GX13    GY13     GX14    GY14    GX15    GY15     
+				__m256i GXYH = _mm256_unpackhi_epi16(GX16, GY16);
+
+				
+				__m256i ResultL = _mm256_cvtps_epi32(_mm256_sqrt_ps(_mm256_cvtepi32_ps(_mm256_madd_epi16(GXYL, GXYL))));
+				__m256i ResultH = _mm256_cvtps_epi32(_mm256_sqrt_ps(_mm256_cvtepi32_ps(_mm256_madd_epi16(GXYH, GXYH))));
+				
+				__m256i Result = _mm256_packus_epi16(_mm256_packus_epi32(ResultL, ResultH), Zero);
+
+				__m128i Ans = _mm256_castsi256_si128(Result);
+				_mm_storeu_si128((__m128i *)(LinePD + X), Ans);
+			}
+
+			for (int X = Block * BlockSize; X < Width * 3; X++)
+			{
+				int GX = First[X] - First[X + 6] + (Second[X] - Second[X + 6]) * 2 + Third[X] - Third[X + 6];
+				int GY = First[X] + First[X + 6] + (First[X + 3] - Third[X + 3]) * 2 - Third[X] - Third[X + 6];
+				LinePD[X] = IM_ClampToByte(sqrtf(GX * GX + GY * GY + 0.0F));
+			}
+		}
+	}
+	free(RowCopy);
+}
+
+
 int main() {
 	Mat src = imread("F:\\car.jpg");
 	int Height = src.rows;
@@ -231,12 +399,12 @@ int main() {
 	int Radius = 11;
 	int Adjustment = 50;
 	int64 st = cvGetTickCount();
-	for (int i = 0; i <50; i++) {
-		Sobel_SSE(Src, Dest, Width, Height, Stride);
-	}
-	double duration = (cv::getTickCount() - st) / cv::getTickFrequency() * 20;
+	/*for (int i = 0; i <1000; i++) {
+		Sobel_SSE3(Src, Dest, Width, Height, Stride);
+	}*/
+	double duration = (cv::getTickCount() - st) / cv::getTickFrequency();
 	printf("%.5f\n", duration);
-	Sobel_SSE(Src, Dest, Width, Height, Stride);
+	Sobel_AVX1(Src, Dest, Width, Height, Stride);
 	Mat dst(Height, Width, CV_8UC3, Dest);
 	imshow("origin", src);
 	imshow("result", dst);
